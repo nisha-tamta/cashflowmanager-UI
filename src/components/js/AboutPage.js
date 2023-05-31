ebbff931ec34343862922095a92ad5ad8e691529
Nisha Tamta <nisha.tamta199@gmail.com>
2023-05-31 14:59:51 +0530

    Edited About page

--- src/components/js/AboutPage.js
+++ src/components/js/AboutPage.js
@@ -7,22 +7,27 @@ const AboutPage = () => {
     <div className="container">
       <NavBar />
       <div className="homescreen-container">
-        <div className="content-container">
-          <div className="content-header">
-            <h1>
-              About Us..
-              <img
-                src="https://pbs.twimg.com/media/EYhrRIGUYAEjmGN.jpg"
-                alt="Expense Tracker Logo"
-                className="login-logo"
-              />
-            </h1>
-          </div>
-          <div className="content-profile-container">
-            <p>Expense Tracker is a user-friendly web-based application designed to simplify expense management. We understand the challenges of budgeting, tracking expenses, and achieving financial goals. Our platform offers comprehensive features, including expense tracking, budget setting, and spending analysis. With a modern and intuitive interface, we prioritize visual appeal and user-friendliness. Take control of your finances, make informed decisions, and work towards your financial goals with Expense Tracker. Start managing your expenses effortlessly today.</p>
-            <p>Experience the power of Expense Tracker today and take control of your financial future.</p>
-            <div className="content-body" />
-          </div>
+        <div className="content-about-container">
+          <header className="content-header">
+            <h1>About Us</h1>
+            <img
+              src="https://pbs.twimg.com/media/EYhrRIGUYAEjmGN.jpg"
+              alt="Expense Tracker Logo"
+              className="login-logo-about"
+            />
+          </header>
+          <section className="content-chat-container">
+            <div className="info-box">
+              <h2>Welcome to Expense Tracker</h2>
+              <p>Expense Tracker is a user-friendly web-based application designed to simplify expense management. We understand the challenges of budgeting, tracking expenses, and achieving financial goals. Our platform offers comprehensive features, including expense tracking, budget setting, and spending analysis. With a modern and intuitive interface, we prioritize visual appeal and user-friendliness.</p>
+
+              <h2>Why Expense Tracker?</h2>
+              <p>Take control of your finances, make informed decisions, and work towards your financial goals with Expense Tracker. Start managing your expenses effortlessly today.</p>
+
+              <h2>Join Us Today</h2>
+              <p>Experience the power of Expense Tracker today and take control of your financial future.</p>
+            </div>
+          </section>
         </div>
       </div>
     </div>
