47da232314385c641e046f927df64de1f5cdb585
Nisha Tamta <nisha.tamta199@gmail.com>
2023-06-08 17:50:31 +0530

    Revert "Role added in CreateUserForm"
    This reverts commit 1de539008567ee8f744c9b4d97141b460b4633aa.

--- src/components/js/CreateUserForm.js
+++ src/components/js/CreateUserForm.js
@@ -14,8 +14,7 @@ const CreateUserForm = () => {
     username: "",
     password: "",
     phoneNumber: "",
-    defaultBudget: "",
-    roleIdInt: 1
+    defaultBudget: ""
   });
 
   const handleChange = (e) => {
@@ -79,19 +78,6 @@ const CreateUserForm = () => {
             <div >
               <form>
                 {<div className="error-message">{error}</div>}
-                <div className="create-chat-input-container">
-                  <label >Role</label>
-                  <select
-                    className="create-chat-input"
-                    name="roleIdInt"
-                    value={user.roleIdInt}
-                    onChange={handleChange}
-                  >
-                    <option className="item-value-expense" value="1">Admin</option>
-                    <option className="item-value-expense" value="2">Manager</option>
-                    <option className="item-value-expense" value="3">Employee</option>
-                  </select>
-                </div>
                 <div className="create-chat-input-container">
                   <label htmlFor="firstName">First Name</label>
                   <input
